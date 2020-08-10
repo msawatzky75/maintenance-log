bc65d102d17043b0872279b9329894436b9d62bd
Matthew Sawatzky <34319870+msawatzky75@users.noreply.github.com>
2020-08-10 17:50:54 -0500

    changed web to nuxt

---
+++ web/pages/login.vue
@@ -0,0 +1,42 @@
+<template>
+	<div>
+		<b-form @submit="userLogin">
+			<div>
+				<label>Username</label>
+				<input v-model="login.username" type="text" />
+			</div>
+			<div>
+				<label>Password</label>
+				<input v-model="login.password" type="text" />
+			</div>
+			<div>
+				<b-button type="submit">Submit</b-button>
+			</div>
+		</b-form>
+	</div>
+</template>
+
+<script>
+export default {
+	data() {
+		return {
+			login: {
+				username: '',
+				password: '',
+			},
+		}
+	},
+	methods: {
+		async userLogin() {
+			try {
+				const response = await this.$auth.loginWith('local', {
+					data: this.login,
+				})
+				console.log(response)
+			} catch (err) {
+				console.log(err)
+			}
+		},
+	},
+}
+</script>
